99094bf851ffb761891f07fed2ab4128c40aae98
Giovanni <giovannilopez9808@gmail.com>
2021-04-01 21:02:11 -0600

    Change on graphics of pollutants and UVI OMI

---
+++ 2000-2019/Scripts/Graphics_pollutants.py
@@ -0,0 +1,125 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+inputs = {
+    "PM10": {
+        "tick": "PM$_{10}$",
+        "color": "#A25715",
+        "title": "PM$_{10}$ ($\mu g/m^3$)",
+        "lim sup": 75,
+        "lim inf": 0,
+        "delta": 15
+    },
+    "CO": {
+        "tick": "CO",
+        "color": "purple",
+        "title": "CO (ppm)",
+        "lim sup": 4,
+        "lim inf": 0,
+        "delta": 1
+    },
+    "NO2": {
+        "tick": "NO$_2$",
+        "color": "blue",
+        "title": "NO$_2$, SO$_2$ (ppb)",
+        "lim sup": 50,
+        "lim inf": 0,
+        "delta": 10
+    },
+    "O3": {
+        "tick": "O$_3$",
+        "color": "green",
+        "title": "O$_3$ (ppb)",
+        "lim sup": 90,
+        "lim inf": 50,
+        "delta": 10
+    },
+    "AOD": {
+        "tick": "AOD$_{340}$",
+        "color": "#CB258C",
+        "title": "AERONET AOD",
+        "lim sup": 0.8,
+        "lim inf": 0,
+        "delta": 0.15
+    },
+    "SO2": {
+        "tick": "SO$_{2}$",
+        "color": "black",
+        "title": "NO$_2$, SO$_2$ (ppb)",
+        "lim sup": 50,
+        "lim inf": 0,
+        "delta": 10
+    },
+}
+dir, arc = "../Archivos/", "CDMX.csv"
+lw = 4
+font_size = 12
+plt.rc('font', size=font_size)
+plt.rc('xtick', labelsize=font_size)
+plt.rc('ytick', labelsize=font_size-1)
+fig, (ax1, ax3, ax4) = plt.subplots(3, figsize=(9, 9))
+plt.subplots_adjust(top=0.97,
+                    bottom=0.1,
+                    left=0.09,
+                    right=0.9,
+                    hspace=0.15)
+ax2 = ax1.twinx()
+ax5 = ax4.twinx()
+axs = np.array([ax1, ax2, ax3, ax4, ax5, ax3])
+for input, ax in zip(inputs, axs):
+    tick = inputs[input]["tick"]
+    color = inputs[input]["color"]
+    title = inputs[input]["title"]
+    lim_inf = inputs[input]["lim inf"]
+    lim_sup = inputs[input]["lim sup"]
+    delta = inputs[input]["delta"]
+    label = np.arange(2000, 2020)
+    x = np.arange(20)
+    if ax != ax5:
+        file = dir+input+"_"+arc
+        data = pd.read_csv(file)
+        mean = list(data.mean())
+    else:
+        year_list, data = np.loadtxt(dir+input+".txt",
+                                     skiprows=1,
+                                     unpack=True)
+        x = year_list-2000
+        label = year_list
+        mean = data
+    fit = np.polyfit(x, mean, 1)
+    prom = round(np.mean(mean), 3)
+    print("{} {:.1f} {:.1f} {:.2f} {:.2f}".format(
+        input, prom, fit[0]*100/prom, fit[0], fit[1]))
+    ax.plot(label, mean,
+            ls="-",
+            label=tick,
+            color=color,
+            linewidth=lw)
+    ax.set_xlim(2000, 2019)
+    ax.set_ylim(lim_inf, lim_sup)
+    yticks = np.arange(lim_inf, lim_sup+delta, delta)
+    ax.set_yticks(yticks)
+    if not(ax in [ax4, ax5]):
+        ax.set_xticks([])
+    else:
+        if ax == ax4:
+            ax.set_xticks(label)
+            ax.set_xticklabels(label,
+                               rotation=60)
+    if ax in [ax2, ax5]:
+        ax.set_ylabel(title,
+                      rotation=-90,
+                      va="bottom")
+    else:
+        ax.set_ylabel(title)
+    if ax in [ax2, ax4]:
+        ax.legend(frameon=False,
+                  ncol=2, loc="best",
+                  bbox_to_anchor=(0.84, 1))
+    else:
+        ax.legend(frameon=False,
+                  ncol=2,
+                  loc="upper right")
+# plt.savefig("../Graficas/contCDMX.png",dpi=300)
+plt.show()
--- 2000-2019/Scripts/UVIOMI.py
+++ 2000-2019/Scripts/UVIOMI.py
@@ -1,58 +1,123 @@
-import numpy as np
-import matplotlib.pyplot as plt
 from matplotlib.colors import LinearSegmentedColormap
+import matplotlib.pyplot as plt
+import numpy as np
 import datetime
-Meses=["January","February","March","April","May","June","July","August"
-            ,"September","October","November","December"]
-numyear=["2005","","","2008","",""
-,"","2012","","","","2016","","","2019"]
-daysnum,resul,prom=np.arange(0,365,30.5),np.zeros([15,365]),np.zeros([12,2])
-date,data=np.loadtxt("../Archivos/UVI-OMI.txt",usecols=0,dtype=str),np.loadtxt("../Archivos/UVI-OMI.txt",usecols=1)
-n=np.size(data)
+
+
+def forceAspect(ax, aspect):
+    im = ax.get_images()
+    extent = im[0].get_extent()
+    ax.set_aspect(abs((extent[1]-extent[0])/(extent[3]-extent[2]))/aspect)
+
+
+def obtain_month_names():
+    names = []
+    for i in range(1, 13):
+        date = datetime.date(2000, i, 1)
+        names.append(date.strftime("%B"))
+    return names
+
+
+numyear = ["2005",
+           "",
+           "",
+           "2008",
+           "",
+           "",
+           "",
+           "2012",
+           "",
+           "",
+           "",
+           "2016",
+           "",
+           "",
+           "2019"]
+month_days = np.arange(0, 365, 30.2)
+month_names = obtain_month_names()
+resul = np.zeros([15, 365])
+prom = np.zeros([12, 2])
+date, data = np.loadtxt("../Archivos/UVI-OMI.txt", usecols=0,
+                        dtype=str), np.loadtxt("../Archivos/UVI-OMI.txt", usecols=1)
+n = np.size(data)
 print("Calculando valores y promedios")
 for i in range(n):
-    year,month,day=int("20"+date[i][0:2]),int(date[i][2:4]),int(date[i][4:6])
-    day=(datetime.date(year,month,day)-datetime.date(year,1,1)).days
-    if day>364: day=364
-    year+=-2005
-    num=data[i]
-    if num!=0:
-        month+=-1
-        prom[month,0]+=num;prom[month,1]+=1
+    year, month, day = int(
+        "20"+date[i][0:2]), int(date[i][2:4]), int(date[i][4:6])
+    day = (datetime.date(year, month, day)-datetime.date(year, 1, 1)).days
+    if day > 364:
+        day = 364
+    year += -2005
+    num = data[i]
+    if num != 0:
+        month += -1
+        prom[month, 0] += num
+        prom[month, 1] += 1
 for month in range(12):
-    prom[month,0]=prom[month,0]/prom[month,1]
+    prom[month, 0] = prom[month, 0]/prom[month, 1]
 for i in range(n):
-    year,month,day=int("20"+date[i][0:2]),int(date[i][2:4]),int(date[i][4:6])
-    day=(datetime.date(year,month,day)-datetime.date(year,1,1)).days
-    month+=-1
-    year+=-2005
-    if day>364: day=364
-    if data[i]==0: data[i]=prom[month,0]
-    resul[year,day]=data[i]
+    year, month, day = int(
+        "20"+date[i][0:2]), int(date[i][2:4]), int(date[i][4:6])
+    day = (datetime.date(year, month, day)-datetime.date(year, 1, 1)).days
+    month += -1
+    year += -2005
+    if day > 364:
+        day = 364
+    if data[i] == 0:
+        data[i] = prom[month, 0]
+    resul[year, day] = data[i]
 for year in range(15):
     for day in range(365):
-        if resul[year,day]==0:
-            n_year=2005+year
-            month=(datetime.date(year,1,1)+datetime.timedelta(days=day)).month
-            resul[year,day]=prom[month,0]
-colors=[(58/255,156/255,43/255),(152/255,196/255,8/255),
-(1,244/255,0),(1,211/255,0),(246/255,174/255,0),(239/255,131/255,0),
-(232/255,97/255,5/255),(255/255,34/255,34/255),
-(230/255,42/255,20/255),(165/255,0/255,0/255),
-(118/255,46/255,159/255),(150/255,53/255,188/255),
-(184/255,150/255,235/255),(198/255,198/255,248/255)]
-n_bin=15
-font_size=12
-cmap_name="UV_Index"
-cm=LinearSegmentedColormap.from_list(cmap_name,colors,N=n_bin)
+        if resul[year, day] == 0:
+            n_year = 2005+year
+            month = (datetime.date(year, 1, 1) +
+                     datetime.timedelta(days=day)).month
+            resul[year, day] = prom[month, 0]
+colors = [(58/255, 156/255, 43/255),
+          (152/255, 196/255, 8/255),
+          (1, 244/255, 0),
+          (1, 211/255, 0),
+          (246 / 255, 174/255, 0),
+          (239/255, 131/255, 0),
+          (232/255, 97/255, 5/255),
+          (255/255, 34/255, 34/255),
+          (230/255, 42/255, 20/255),
+          (165/255, 0/255, 0/255),
+          (118/255, 46/255, 159/255),
+          (150/255, 53/255, 188/255),
+          (184/255, 150/255, 235/255),
+          (198/255, 198/255, 248/255)]
+n_bin = 15
+font_size = 12
+cmap_name = "UV_Index"
+cm = LinearSegmentedColormap.from_list(cmap_name, colors, N=n_bin)
 print("Graficando UV Index")
-plt.subplots_adjust(left=0.094,right=0.977,bottom=0.205,top=0.89)
-plt.title("UV Index satellite-derived in Mexico City \n Period 2005-2019")
-plt.yticks(np.arange(np.size(numyear)),numyear,fontsize=font_size)
-plt.xticks(daysnum,Meses,rotation=60,fontsize=font_size)
-levels=np.arange(1,16,1)
-plt.contourf(resul,levels=levels,cmap=cm)
-plt.grid(linewidth=1,color="black",linestyle="--")
-cbar=plt.colorbar(ticks=np.arange(1,16,1))
-cbar.ax.set_ylabel("UV Index",rotation=-90,va="bottom",fontsize=11)
-plt.savefig("../Graficas/UVI-OMI.eps",dpi=300)
+fig, ax = plt.subplots(1, 1)
+plt.subplots_adjust(left=0.094,
+                    right=0.977,
+                    bottom=0.205,
+                    top=0.89)
+ax.set_ylim(0, 14)
+ax.set_yticks(np.arange(np.size(numyear)))
+ax.set_yticklabels(numyear,
+                   fontsize=font_size)
+ax.set_xticks(month_days)
+ax.set_xticklabels(month_names,
+                   rotation=60,
+                   fontsize=font_size)
+map = ax.imshow(resul,
+                cmap=cm,
+                vmin=1,
+                vmax=15,
+                origin="lower")
+ax.grid(linewidth=1,
+        color="black",
+        linestyle="--")
+forceAspect(ax, 1.5)
+cbar = fig.colorbar(map, ticks=np.arange(1, 16, 1))
+cbar.ax.set_ylabel("UV Index",
+                   rotation=-90,
+                   va="bottom",
+                   fontsize=11)
+ax.set_title("UV Index satellite-derived in Mexico City \n Period 2005-2019")
+plt.savefig("../Graficas/UVI-OMI.png", dpi=300)
