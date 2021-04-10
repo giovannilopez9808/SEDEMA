860a6ac2554a6d4a686af98bf91110a318af0ec6
gio <giovannilopez9808@gmail.com>
2021-04-09 23:27:57 -0500

    Graphics per day

---
+++ 2000-2019/Scripts/graphics_day.py
@@ -0,0 +1,64 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+
+inputs = {
+    "path stations": "../Stations/",
+    "date": "190803",
+}
+colors = [
+    "#03071e",
+    "#6a040f",
+    "#dc2f02",
+    "#219ebc",
+    "#06d6a0",
+    "#52b788",
+    "#2d6a4f",
+    "#1b4332",
+    "#b07d62",
+    "#774936",
+    "#99d98c",
+    "#7400b8",
+    "#ff0a54"
+]
+colors = {
+    "CHO": "Blue",
+    "CUT": "#219ebc",
+    "LAA": "Red",
+    "FAC":  "#774936",
+    "SFE": "Green",
+    "MON": "Purple",
+    "SAG": "orange",
+    "PED": "#f89edf",
+    "MER": "black",
+}
+stations = sorted(os.listdir(inputs["path stations"]))
+for station in stations:
+    path = inputs["path stations"]+station+"/Erythemal/"
+    try:
+        hour, data = np.loadtxt(path+inputs["date"]+".txt",
+                                unpack=True)
+        if np.mean(data) != 0:
+            color = colors[station]
+            plt.plot(hour, data*40,
+                     label=station,
+                     marker="o",
+                     color=color)
+    except:
+        pass
+plt.text(12, 17, "3 august 2019", fontsize=12)
+plt.ylabel("UV Index")
+plt.xlabel("Local time (h)")
+plt.xlim(5, 20)
+plt.ylim(0, 16)
+plt.yticks(np.arange(0, 17))
+plt.xticks(np.arange(5, 21))
+plt.grid(ls="--",
+         color="#000000",
+         alpha=0.5)
+plt.legend(frameon=False,
+           ncol=8,
+           fontsize=8,
+           bbox_to_anchor=(1.04, 1.01, 0.05, 0.05)
+           )
+plt.show()
